207b066df6fb28783786cef9d5fee63e52e26c97
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2024-09-12 14:50:59 +0000

    execute code asap

--- safari-mobile-prevent-zoom-on-input-tap/safari-mobile-prevent-zoom-on-input-tap.user.js
+++ safari-mobile-prevent-zoom-on-input-tap/safari-mobile-prevent-zoom-on-input-tap.user.js
@@ -3,15 +3,15 @@
 // @match       https://*/*
 // @grant       none
 // @version     1.0
-// @downloadURL     https://github.com/danielvigaru/userscripts/raw/main/safari-mobile-prevent-zoom-on-input-tap/safari-mobile-prevent-zoom-on-input-tap.user.js
-// @updateURL       https://github.com/danielvigaru/userscripts/raw/main/safari-mobile-prevent-zoom-on-input-tap/safari-mobile-prevent-zoom-on-input-tap.user.js
-// @homepageURL     https://github.com/danielvigaru/userscripts/tree/main/safari-mobile-prevent-zoom-on-input-tap
+// @downloadURL https://github.com/danielvigaru/userscripts/raw/main/safari-mobile-prevent-zoom-on-input-tap/safari-mobile-prevent-zoom-on-input-tap.user.js
+// @updateURL   https://github.com/danielvigaru/userscripts/raw/main/safari-mobile-prevent-zoom-on-input-tap/safari-mobile-prevent-zoom-on-input-tap.user.js
+// @homepageURL https://github.com/danielvigaru/userscripts/tree/main/safari-mobile-prevent-zoom-on-input-tap
 // ==/UserScript==
 
-window.addEventListener('load', () => {
+(function() {
     const scaleMeta = document.createElement('meta');
     scaleMeta.setAttribute('name', 'viewport');
     scaleMeta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1');
 
     document.head.append(scaleMeta);
-});
+})();
